cf7cc56916a69e0dc7648df4cd2969df4db42d90
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-02-04 10:12:50 +0000

    hf token removed

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
 
     audio_processing_path = os.path.join("facebook", "wav2vec2-base-960h")
     telegram_bot_token = os.getenv('BOT_TOKEN')
-    huggingface_token = os.getenv('CACTUS_TOKEN')
     gemini_token = os.getenv('GEMINI_TOKEN')
 
     assistant = AssistantManager(audio_processing_path=audio_processing_path,
