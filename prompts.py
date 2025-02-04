620071cccbd211d00b9c072af3342866349f8c70
GianM0027 <104518262+GianM0027@users.noreply.github.com>
2025-02-04 09:59:07 +0000

    prompts update

--- prompts.py
+++ prompts.py
@@ -4,8 +4,15 @@ INITIAL_GREETING = (
     "\n- Set the address of the connection your cactus assistant will be connected to."
     "\n- Chat with the assistant and obtain written responses."
     "\n- Set the behavior of your assistant."
-    "\n- Access, modify or delete the assistant behavioral prompts and memory.")
+    "\n- Access, modify or delete the assistant behavioral prompts and memory.json."
+    "\n\n How can I call you?")
 
-CACTUS_INSTRUCTIONS = \
-    "You are a smart desk assistant, you are cactus-shaped and you always reply in a concise and polite manner."
+CACTUS_BASE_INSTRUCTIONS = (
+    "You are a smart desk assistant, you are cactus-shaped and you are always very useful."
+    "Always answer with plain text, with no markdown syntax, you are only allowed to make bullet points if necessary, nothing more")
 
+ASK_INITIALIZATION_PROMPT = "Please, enter your initialization prompt"
+ASK_USERNAME = "Please, enter your new username"
+
+INITIALIZATION_PROMPT_CONFIRMATION = "New initialization prompt set! From now on, I will behave according to your instructions. \nHow can I help you?"
+USERNAME_CONFIRMATION = "Your new username is set! \nHow can I help you?"
